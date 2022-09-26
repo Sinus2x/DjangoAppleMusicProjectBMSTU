d9e410fa62f2fdc9708a7ad53e11408395c80d35
Sinus2x <hatin.max2002@gmail.com>
2022-09-27 01:03:19 +0300

    init commit

---
+++ bmstu_lab/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import date
+
+
+def hello(request):
+    return render(request, 'index.html', { 'data' : {
+        'current_date': date.today(),
+        'list': ['python', 'django', 'html']
+    }})
+
+
+def GetOrders(request):
+    return render(request, 'orders.html', {'data' : {
+        'current_date': date.today(),
+        'artists': [
+            {'title': 'AC/DC', 'id': 1},
+            {'title': 'Nirvana', 'id': 2},
+            {'title': 'Led Zeppelin', 'id': 3},
+        ]
+    }})
+
+
+def GetOrder(request, id):
+    return render(request, 'order.html', {'data' : {
+        'current_date': date.today(),
+        'id': id
+    }})
