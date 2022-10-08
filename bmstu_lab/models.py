57e60911238c9d7299b7817f1aa72e533bb7de56
Sinus2x <hatin.max2002@gmail.com>
2022-10-08 13:38:52 +0300

    connected orm

--- bmstu_lab/models.py
+++ bmstu_lab/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Albums(models.Model):
+    album_id = models.AutoField(primary_key=True)
+    n_songs = models.IntegerField()
+    artist = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'albums'
+
+
+class Customers(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'customers'
+
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customers, models.DO_NOTHING)
+    album_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'orders'
--- bmstu_lab/views.py
+++ bmstu_lab/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import date
 
 
 def hello(request):
-    return render(request, 'index.html', { 'data' : {
+    return render(request, 'index.html', {'data': {
         'current_date': date.today(),
         'list': ['python', 'django', 'html']
     }})
