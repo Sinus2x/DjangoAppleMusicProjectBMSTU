5a6e8741fcd97682688098da1dc15025ad957617
Sinus2x <hatin.max2002@gmail.com>
2022-12-14 14:59:04 +0300

    7 lab

--- bmstu_lab/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Albums, Orders, Customers, AlbumsOrders
-
-admin.site.register(Albums)
-admin.site.register(Orders)
-admin.site.register(Customers)
-admin.site.register(AlbumsOrders)
